b0c17efe1bc07a1f410763e15683b2f24cbf7e3f
pr0n1x <rootfavell@gmail.com>
2013-02-13 19:28:05 +0800

    добавлен дефолтный шаблон компонента obx:layout

---
+++ install/components/obx/layout/templates/.default/component_epilog.php
@@ -0,0 +1,11 @@
+<?php
+/*****************************************
+ ** @vendor A68 Studio                  **
+ ** @mailto info@a-68.ru                **
+ ** @time 17:37                       **
+ ** @user tashiro                       **
+ *****************************************/
+
+OBX_Tools::addComponentLess(__FILE__);
+
+?>
