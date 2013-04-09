38103f62e0c66ec9e9e32cb1ca1beb3dc6fe2c76
tashiro <Tashiro@ya.ru>
2013-04-09 19:33:15 +0800

    Оптимизировал ф-ию OBX_Tools::rusQuantity(); Теперь она может принимать только один аргумент в простых случаях типа: час, часа, часов. Для сложных типа: день, дня , дней - необходимо использовать все аргусенты ф-ии

--- classes/Tools.php
+++ classes/Tools.php
@@ -144,19 +144,23 @@ class OBX_Tools
 	/**
 	 * Отдает наименование единицы в форме соотвествующей числу
 	 * @param integer $quantity число
-	 * @param string $nominative именительный подеж ед. число
-	 * @param string $genetive родительный подеж ед. число
-	 * @param string $genplural родительный подеж ед. число
+	 * @param string $nominative именительный подеж ед. число (час)
+	 * @param string $genetive родительный подеж ед. число  (часа)
+	 * @param string $genplural родительный подеж мн. число (часов)
 	 * @return string
 	 */
-	static public function rusQuantity($quantity, $nominative, $genetive, $genplural){
+	static public function rusQuantity($quantity, $nominative, $genetive = NULL, $genplural = NULL){
+		$oneState = false;
+		if($genetive == NULL || $genplural = NULL) {
+			$oneState = true;
+		}
 		$quantity = abs(intval($quantity));
 		switch($quantity%100) {
 			case 11:
 			case 12:
 			case 13:
 			case 14:
-				return $genplural;
+				return ($oneState)?$nominative."ов":$genplural;
 				break;
 			default:
 				switch($quantity%10) {
@@ -166,10 +170,10 @@ class OBX_Tools
 					case 2:
 					case 3:
 					case 4:
-						return $genetive;
+						return ($oneState)?$nominative."а":$genetive;
 						break;
 					default:
-						return $genplural;
+						return ($oneState)?$nominative."ов":$genplural;
 				}
 		}
 	}
