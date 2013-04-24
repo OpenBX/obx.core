4a65bce10d17d70764618f8e7d077bc8d638a095
pr0n1x <rootfavell@gmail.com>
2013-04-24 12:40:53 +0800

    Добаивл короткое описание метода obx.core: OBX\DBSimple

--- classes/DBSimple.php
+++ classes/DBSimple.php
@@ -350,7 +350,28 @@ abstract class OBX_DBSimple extends OBX_CMessagePoolDecorator
 	protected $_arSortDefault = array('ID' => 'ASC');
 
 	/**
-	 * TODO: Написать описание
+	 * Типы и атрибуты полей основной таблицы сущности
+	 * Используется для проверки входных данных в методах $this->add() и $this->update()
+	 * Выше все контстанты используемые в этом массиве документированы
+	 * Пример:
+	 * <code>
+	 * 	<?php
+	 * 		$this->_arTableFieldsCheck = array(
+	 * 			'ID' => self::FLD_T_INT | self::FLD_NOT_NULL,
+	 * 			'DATE_CREATED' => self::FLD_T_NO_CHECK,
+	 * 			'TIMESTAMP_X' => self::FLD_T_NO_CHECK,
+	 * 			'USER_ID' => self::FLD_T_USER_ID | self::FLD_NOT_NULL | self::FLD_DEFAULT | self::FLD_REQUIRED,
+	 * 			'STATUS_ID' => self::FLD_T_INT | self::FLD_NOT_NULL | self::FLD_DEFAULT | self::FLD_REQUIRED,
+	 * 			'CURRENCY' => self::FLD_T_CODE | self::FLD_NOT_NULL | self::FLD_DEFAULT | self::FLD_REQUIRED,
+	 * 			'DELIVERY_ID' => self::FLD_T_INT,
+	 * 			'DELIVERY_COST' => self::FLD_T_FLOAT,
+	 * 			'PAY_ID' => self::FLD_T_INT,
+	 * 			'PAY_TAX_VALUE' => self::FLD_T_FLOAT,
+	 * 			'DISCOUNT_ID' => self::FLD_T_INT,
+	 * 			'DISCOUNT_VALUE' => self::FLD_T_FLOAT
+	 * 		);
+	 * 	?>
+	 * </code>
 	 * @var array
 	 * @access protected
 	 */
