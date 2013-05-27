037ee1018299e7e474f29eaa1e4ae6be57955c30
pr0n1x <rootfavell@gmail.com>
2013-05-28 00:49:57 +0800

    Добавил правильное имя :)

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,16 +1,5 @@
-<?
-/*************************************
- ** @product OBX:Core Bitrix Module **
- ** @authors                        **
- **         Maksim S. Makarov       **
- **         Morozov P. Artem        **
- ** @License GPLv3                  **
- ** @mailto rootfavell@gmail.com    **
- ** @mailto tashiro@yandex.ru       **
- *************************************/
-
-$MESS["OBX_MODULE_INSTALL_NAME"] = "obx.core";
-$MESS["OBX_MODULE_INSTALL_DESCRIPTION"] = 'Базовый модуль OBX';
-$MESS["OBX_PARTNER_NAME"] = "Студия A68";
+<?php
+$MESS["OBX_MODULE_INSTALL_NAME"] = "DevTop:: Открытые решения: «Базовый модуль»";
+$MESS["OBX_MODULE_INSTALL_DESCRIPTION"] = 'Базовый модуль';
+$MESS["OBX_PARTNER_NAME"] = "А-68 - DevTop";
 $MESS["OBX_PARTNER_URI"] = "http://www.a-68.ru";
-?>
