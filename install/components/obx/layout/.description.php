8041c4776cf1bcc5d8c4edc4036ad8c570877a5a
pr0n1x <rootfavell@gmail.com>
2013-05-29 19:53:44 +0800

    Добаивл описание для прохождения контроля качества

---
+++ install/components/obx/layout/.description.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => 'Component for simplify layout coding',
+	"DESCRIPTION" => 'Component for simplify layout coding',
+	"ICON" => "/images/breadcrumb.gif",
+	"PATH" => array(
+		"ID" => "utility",
+	),
+);
--- install/php_interface/event.d/obx.core.debug.php
+++ install/php_interface/event.d/obx.core.debug.php
@@ -9,19 +9,21 @@
  ** @mailto tashiro@yandex.ru       **
  *************************************/
 
+use OBX\Core\Tools;
+
 if( !function_exists('wd') ) {
 	function wd($mixed, $collapse = null, $bPrint = true) {
-		return OBX_Tools::debug($mixed, $collapse, $bPrint);
+		return Tools::debug($mixed, $collapse, $bPrint);
 	}
 }
 if( !function_exists('d') ) {
 	function d($mixed, $collapse = null, $bPrint = true) {
-		return OBX_Tools::debug($mixed, $collapse, $bPrint);
+		return Tools::debug($mixed, $collapse, $bPrint);
 	}
 }
 if( !function_exists('dd') ) {
 	function dd($mixed, $collapse = null, $bPrint = true) {
-		return OBX_Tools::debug($mixed, $collapse, $bPrint);
+		return Tools::debug($mixed, $collapse, $bPrint);
 	}
 }
 ?>
