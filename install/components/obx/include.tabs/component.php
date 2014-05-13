e379db205d9ae2b109914d7d217625748804a196
tashiro <tashiro@ya.ru>
2014-05-13 14:48:51 +0800

    Подвкладки в include:tabs

--- install/components/obx/include.tabs/component.php
+++ install/components/obx/include.tabs/component.php
@@ -10,6 +10,22 @@ for($iTab=1; $iTab <= $arParams['TABS_COUNT']; $iTab++) {
 	if( array_key_exists($arParams['TAB_'.$iTab.'_ID'], $arResult['TABS']) ) {
 		ShowError('В параметрах компонента задано более одной вкладки с ID "'.$arParams['TAB_'.$iTab.'_ID'].'"');
 	}
+    $arChilds = array();
+    if (intval($arParams['TAB_'.$iTab.'_CAT_COUNT']) > 0) {
+        for ($i=0;$i++ < intval($arParams['TAB_'.$iTab.'_CAT_COUNT']);) {
+            $arChilds[$i]['NAME'] = $arParams['TAB_'.$iTab.'_CAT_'.$i];
+
+            $arChilds[$i]['MAIN_INCLUDE_PARAMS'] = array(
+                'AREA_FILE_SHOW' => 'sect',
+                'AREA_FILE_SUFFIX' => $arParams['TAB_'.$iTab.'_CAT_ID_'.$i],
+                'AREA_FILE_RECURSIVE' => 'N',
+                'NAME' => $arParams['TAB_'.$iTab.'_CAT_'.$i],
+                'EDIT_TEMPLATE' => ''
+            );
+            $arChilds[$i]['ID'] = $arParams['TAB_'.$iTab.'_CAT_ID_'.$i];
+            $arChilds[$i]['URL'] = str_replace('#TAB_ID#', $arParams['TAB_'.$iTab.'_CAT_ID_'.$i], $arParams['URL_TEMPLATE']);
+        }
+    }
 	$arResult['TABS'][$arParams['TAB_'.$iTab.'_ID']] = array(
 		'ID' => $arParams['TAB_'.$iTab.'_ID'],
 		'NUMBER' => $iTab,
@@ -21,8 +37,10 @@ for($iTab=1; $iTab <= $arParams['TABS_COUNT']; $iTab++) {
 			'AREA_FILE_SHOW' => 'sect',
 			'AREA_FILE_SUFFIX' => $arParams['TAB_'.$iTab.'_ID'],
 			'AREA_FILE_RECURSIVE' => $arParams['TAB_'.$iTab.'_RCV'],
+                    'NAME' => $arParams['TAB_' . $iTab . '_CAPTION'],
 			'EDIT_TEMPLATE' => ''
-		)
+            ),
+            'CHILDS' => $arChilds
 	);
 }
 /** @var \CBitrixComponent $this */
