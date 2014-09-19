4f808ff005046bb5b8ccf8c3227ff70179a2f2eb
pr0n1x <rootfavell@gmail.com>
2014-09-19 18:07:31 +0800

    версия следующего релиза 1.2.5. Поправил ссылку на разрабочтика модуля

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -2,4 +2,4 @@
 $MESS["OBX_MODULE_INSTALL_NAME"] = "OpenBX: Базовый модуль";
 $MESS["OBX_MODULE_INSTALL_DESCRIPTION"] = 'Базовый модуль';
 $MESS["OBX_PARTNER_NAME"] = "OpenBitrixSolutions";
-$MESS["OBX_PARTNER_URI"] = "http://www.a-68.ru";
+$MESS["OBX_PARTNER_URI"] = "http://www.devtop.ru";
