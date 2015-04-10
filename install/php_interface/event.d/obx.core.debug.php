b564934ed628a659a521197a27fa74ff225deea0
pr0n1x <pronix@devtop.ru>
2015-04-10 12:35:06 +0700

    Что бы шторм не отсвечивал...

--- install/php_interface/event.d/obx.core.debug.php
+++ install/php_interface/event.d/obx.core.debug.php
@@ -1,26 +1,28 @@
 <?php
-/**
- * @product OBX:Core Bitrix Module
- * @author Maksim S. Makarov aka pr0n1x
- * @license Affero GPLv3
- * @mailto rootfavell@gmail.com
- * @copyright 2013 Devtop
- */
+/*************************************
+ ** @product OBX:Core Bitrix Module **
+ ** @authors                        **
+ **         Maksim S. Makarov       **
+ **         Morozov P. Artem        **
+ ** @license Affero GPLv3           **
+ ** @mailto rootfavell@gmail.com    **
+ ** @mailto tashiro@yandex.ru       **
+ *************************************/
 
 use OBX\Core\Tools;
 
 if( !function_exists('wd') ) {
 	function wd($mixed, $collapse = null, $bPrint = true) {
-		Tools::debug($mixed, $collapse, $bPrint);
+		return Tools::debug($mixed, $collapse, $bPrint);
 	}
 }
 if( !function_exists('d') ) {
 	function d($mixed, $collapse = null, $bPrint = true) {
-		Tools::debug($mixed, $collapse, $bPrint);
+		return Tools::debug($mixed, $collapse, $bPrint);
 	}
 }
 if( !function_exists('dd') ) {
 	function dd($mixed, $collapse = null, $bPrint = true) {
-		Tools::debug($mixed, $collapse, $bPrint);
+		return Tools::debug($mixed, $collapse, $bPrint);
 	}
 }
