52474d28d810f4108091f1a325396b3fe0cd3f23
pr0n1x <pr0n1x@yandex.ru>
2017-09-21 22:32:03 +0700

    В класс OBX\Core\Components\Ajax добавил поддержку работы в режиме сброса кеша, при котором параметы ajax-вызовов компонентов сохраняются в сессию

--- lib/components/ajax.php
+++ lib/components/ajax.php
@@ -40,14 +40,22 @@ class Ajax {
 	private $_additionalData = null;
 	private $_useTildaKeys = false;
 	private $_isAjaxHitNow = false;
+	private $_cacheType = 'A';
+	private $_cacheTime = 3600;
 	static private $cache = null;
-	const CACHE_TTL = 3600;
+	const CACHE_AUTO_TTL = 36000000;
+	const CACHE_SESSION_TTL = 300;
 	const CACHE_INIT_DIR = 'ajax_call';
 	const AJAX_CALL_PARAMS_MARKER = 'COMPONENT_AJAX_CALL';
 
 	private function __construct($component, $actualFields = null, $useTildaKeys = false, $fillDummyObjectByArray = false) {
 		$this->_params = array();
-		list($this->_name, $this->_template, $arParams) = self::getComponentData($component);
+		list($this->_name,
+			$this->_template,
+			$arParams,
+			$this->_cacheType,
+			$this->_cacheTime
+			) = self::getComponentData($component);
 		$this->_useTildaKeys = !!$useTildaKeys;
 		if( array_key_exists(self::AJAX_CALL_PARAMS_MARKER, $arParams)
 			&& $arParams[self::AJAX_CALL_PARAMS_MARKER] == 'Y'
@@ -93,6 +101,7 @@ class Ajax {
 			}
 			$this->_callId = md5(SITE_ID.':'.$this->_name.':'.$this->_template.':'.$actualParamsSerialized);
 		}
+		self::clearSavedOutdatedSessionParams();
 	}
 
 	public function __get($name) {
@@ -103,6 +112,8 @@ class Ajax {
 			case 'isAjaxHitNow': return $this->_isAjaxHitNow;
 			case 'useTildaKeys': return $this->_useTildaKeys;
 			case 'params': return $this->_params;
+			case 'cacheTime': return $this->_cacheTime;
+			case 'cacheDir': return $this->_cacheDir;
 			case 'actualParams': return $this->_actualParams;
 			case 'additionalData': return $this->_additionalData;
 			default:
@@ -124,25 +135,68 @@ class Ajax {
 
 	public function saveParams() {
 		$cache = self::getCache();
+		$cachedParams = [
+			'callId' => $this->_callId,
+			'name' => $this->_name,
+			'template' => $this->_template,
+			'useTildaKeys' => $this->_useTildaKeys,
+			'actualParams' => $this->_actualParams,
+			'params' => $this->_params,
+			'additionalData' => $this->_additionalData
+		];
 		if( ! $this->isAjaxHitNow &&
-			$cache->startDataCache(self::CACHE_TTL, $this->_callId, self::CACHE_INIT_DIR)
+			$this->_cacheTime > 0 &&
+			$cache->startDataCache($this->_cacheTime+60, $this->_callId, self::CACHE_INIT_DIR)
 		) {
 			/** @noinspection PhpParamsInspection */
-			$cache->endDataCache([
-				'callId' => $this->_callId,
-				'name' => $this->_name,
-				'template' => $this->_template,
-				'useTildaKeys' => $this->_useTildaKeys,
-				'actualParams' => $this->_actualParams,
-				'params' => $this->_params,
-				'additionalData' => $this->_additionalData
-			]);
+			$cache->endDataCache($cachedParams);
+		}
+		if( ! $this->isAjaxHitNow
+			&& isset($_REQUEST['clear_cache'])
+			&& 'Y' === $_REQUEST['clear_cache']
+		) {
+			self::saveParamsToSession($cachedParams);
 		}
 	}
 
-	static public function prepare($component, $actualFields = null, $useTildaKeys = false, $immadiateSaveToCache = false) {
+	static private function saveParamsToSession(&$cachedParams) {
+		if( !isset($_SESSION['OBX_AJAX_CALL_PARAMS'])
+			|| !is_array($_SESSION['OBX_AJAX_CALL_PARAMS']) ) {
+			$_SESSION['OBX_AJAX_CALL_PARAMS'] = [];
+		}
+		$_SESSION['OBX_AJAX_CALL_PARAMS'][$cachedParams['callId']] = $cachedParams;
+		$_SESSION['OBX_AJAX_CALL_PARAMS'][$cachedParams['callId']]['timeout'] = time()+self::CACHE_SESSION_TTL;
+	}
+
+	static public function _updateSessionParamsTimeout($callId) {
+		if( !isset($_SESSION['OBX_AJAX_CALL_PARAMS'])
+			|| !is_array($_SESSION['OBX_AJAX_CALL_PARAMS'])
+			|| empty($_SESSION['OBX_AJAX_CALL_PARAMS'][$callId]['timeout'])
+		) {
+			return false;
+		}
+		$_SESSION['OBX_AJAX_CALL_PARAMS'][$callId]['timeout'] = time()+self::CACHE_SESSION_TTL;
+		return true;
+	}
+
+	public function updateSessionParamsTimeout() {
+		self::_updateSessionParamsTimeout($this->_callId);
+	}
+
+	static public function clearSavedOutdatedSessionParams() {
+		if( isset($_SESSION['OBX_AJAX_CALL_PARAMS'])
+			&& is_array($_SESSION['OBX_AJAX_CALL_PARAMS']) ) {
+			foreach($_SESSION['OBX_AJAX_CALL_PARAMS'] as $callId => $cachedParams) {
+				if( time() >= intval($cachedParams['timeout'])) {
+					unset($_SESSION['OBX_AJAX_CALL_PARAMS'][$callId]);
+				}
+			}
+		}
+	}
+
+	static public function prepare($component, $actualFields = null, $useTildaKeys = false, $immediateSaveToCache = false) {
 		$ajax = new self($component, $actualFields, $useTildaKeys);
-		if( true === $immadiateSaveToCache ) {
+		if( true === $immediateSaveToCache ) {
 			$ajax->saveParams();
 		}
 		return $ajax;
@@ -157,8 +211,16 @@ class Ajax {
 
 	static public function getByCallId($callId) {
 		$cache = self::getCache();
-		if( $cache->initCache(self::CACHE_TTL, $callId, self::CACHE_INIT_DIR) ) {
+		$componentData = null;
+		if( $cache->initCache(self::CACHE_AUTO_TTL, $callId, self::CACHE_INIT_DIR) ) {
 			$componentData = $cache->getVars();
+		}
+		elseif( array_key_exists($callId, $_SESSION['OBX_AJAX_CALL_PARAMS'])
+			&& !empty($_SESSION['OBX_AJAX_CALL_PARAMS'][$callId])
+		) {
+			$componentData = $_SESSION['OBX_AJAX_CALL_PARAMS'][$callId];
+		}
+		if( !empty($componentData) ) {
 			return new self(
 				$componentData,
 				$componentData['actualParams'],
@@ -173,6 +235,9 @@ class Ajax {
 		$name = null;
 		$template = null;
 		$arParams = null;
+		$cacheType = null;
+		$cacheTime = null;
+
 		if($component instanceof CBitrixComponent) {
 			$name = $component->getName();
 			$template = $component->getTemplateName();
@@ -203,10 +268,17 @@ class Ajax {
 		else {
 			throw new ArgumentNullException(Loc::getMessage('OBX_CMP_AJAX_WRONG_ARG_CMP'));
 		}
+		$cacheTime = intval($arParams['CACHE_TIME']);
+		$cacheType = isset($arParams['CACHE_TYPE'])?$arParams['CACHE_TYPE']:'A';
+		if( $cacheTime <= 0 && 'A' === $cacheType ) {
+			$cacheTime = self::CACHE_AUTO_TTL;
+		}
 		return [
 			$name,
 			$template,
-			$arParams
+			$arParams,
+			$cacheType,
+			$cacheTime
 		];
 	}
 }
